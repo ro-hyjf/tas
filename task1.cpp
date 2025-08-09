126050db32d09f15f668719496c18d84ec20296a
Roka <roka15941@gmail.com>
2025-08-09 17:48:04 +0300

    Update and rename README.md to task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+ using namespace std;
+ #define ll long long
+ int main(){
+  int num=2;
+  for(int i=1;i<=10;i++){
+     cout<<num<<"\t";
+    num+=2;
+   
+  }
+   
+   
+ }
